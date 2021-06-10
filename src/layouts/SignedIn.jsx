97176b3b2dc2762d4fbf085a5a88b5af4cc3ffbe
sezginsezen <szgszn@yandex.com>
2021-06-10 10:49:58 +0300

    Commit

---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Dropdown, Image, Menu } from 'semantic-ui-react'
+
+export default function SignedIn() {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://img2.pngindir.com/20180402/bje/kisspng-computer-icons-avatar-login-user-avatar-5ac207e69ecd41.2588125315226654466505.jpg" />
+                <Dropdown pointing="top left" text="Sezgin">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info" />
+                        <Dropdown.Item text="Çıkış Yap" icon="sign-out" />
+
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+        </div>
+    )
+}
